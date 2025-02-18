98f7f06809405dba7e0065eef304d895940b0f44
PseudoLW <pseudolw@gmail.com>
2025-02-18 14:42:53 +0700

    creating a worker

--- src/index.ts
+++ src/index.ts
@@ -43,27 +43,40 @@ namespace Algos {
 
 namespace UI {
     export async function promptInputSrc() {
-        const inp = document.createElement('input');
-        inp.type = 'file';
-        document.body.appendChild(inp);
+        const imageInput = document.createElement('input');
+        const colorCountInput = document.createElement('input');
+        const colorCountLabel = document.createElement('label');
+
+        imageInput.type = 'file';
+
+        colorCountLabel.textContent = 'Color count:';
+
+        colorCountInput.type = 'number';
+        colorCountInput.value = '8';
+        colorCountInput.min = '2';
+
+        document.body.appendChild(imageInput);
+        document.body.appendChild(colorCountLabel);
+        document.body.appendChild(colorCountInput);
+
         await new Promise((res) => {
-            inp.addEventListener('change', res);
+            imageInput.addEventListener('change', res);
         });
-        document.body.removeChild(inp);
-        const file = inp.files![0];
+        const colorCount = colorCountInput.valueAsNumber;
+        document.body.removeChild(imageInput);
+        document.body.removeChild(colorCountLabel);
+        document.body.removeChild(colorCountInput);
+
+        const file = imageInput.files![0];
         const reader = new FileReader();
 
-        return await new Promise<string>((res) => {
-            reader.addEventListener('load', () => res(reader.result as string));
+        return await new Promise<{ src: string, colorCount: number; }>((res) => {
+            reader.addEventListener('load', () => res({
+                src: reader.result as string,
+                colorCount
+            }));
             reader.readAsDataURL(file);
         });
-    }
-
-    export function createLabel(text: string) {
-        const el = document.createElement('div');
-        el.textContent = text;
-        document.body.appendChild(el);
-        return el;
     }
 }
 
@@ -104,26 +117,12 @@ namespace OKlab {
         let l = 0.4122214708 * rr + 0.5363325363 * gg + 0.0514459929 * bb;
         let m = 0.2119034982 * rr + 0.6806995451 * gg + 0.1073969566 * bb;
         let s = 0.0883024619 * rr + 0.2817188376 * gg + 0.6299787005 * bb;
-        // Math.crb (cube root) here is the equivalent of the C++ cbrtf function here: https://bottosson.github.io/posts/oklab/#converting-from-linear-srgb-to-oklab
         l = Math.cbrt(l); m = Math.cbrt(m); s = Math.cbrt(s);
         return [
             l * +0.2104542553 + m * +0.7936177850 + s * -0.0040720468,
             l * +1.9779984951 + m * -2.4285922050 + s * +0.4505937099,
             l * +0.0259040371 + m * +0.7827717662 + s * -0.8086757660
         ] as Triple;
-    }
-    export function toRgb(L: number, A: number, B: number) {
-        let l = L + A * +0.3963377774 + B * +0.2158037573;
-        let m = L + A * -0.1055613458 + B * -0.0638541728;
-        let s = L + A * -0.0894841775 + B * -1.2914855480;
-        l = l ** 3; m = m ** 3; s = s ** 3;
-        let rr = l * +4.0767416621 + m * -3.3077115913 + s * +0.2309699292;
-        let gg = l * -1.2684380046 + m * +2.6097574011 + s * -0.3413193965;
-        let bb = l * -0.0041960863 + m * -0.7034186147 + s * +1.7076147010;
-        return [rr, gg, bb].map(c => {
-            const c01 = c <= 0.0031308 ? c * 12.92 : 1.055 * Math.pow(c, 1 / 2.4) - 0.055;
-            return Math.max(Math.min(Math.round(c01 * 255), 255), 0);
-        }) as Triple;
     }
 }
 namespace ColorPacker {
@@ -138,51 +137,75 @@ namespace ColorPacker {
     }
 }
 
-
-function countColors(data: Uint8ClampedArray) {
-    const colorCounts: Map<number, number> = new Map();
-    for (let i = 0; i < data.length; i += 4) {
-        const [r, g, b] = data.slice(i, i + 3);
-        const colorKey = ColorPacker.pack(r, g, b);
-        colorCounts.set(colorKey, (colorCounts.get(colorKey) ?? 0) + 1);
-    }
-
-    return colorCounts;
-}
-
 type ColorData = {
     count: number;
-    rgbColor: Triple;
     oklabColor: Triple;
 };
+
 function indexColors(data: Uint8ClampedArray) {
     const colorCodeToIndex = new Map<number, number>();
     const colorData = <ColorData[]>[];
     const indexArr = new Array<number>(data.length / 4);
     for (let i = 0; i < data.length; i += 4) {
-        const [r, g, b] = data.slice(i, i + 3);
+        const [r, g, b, a] = data.slice(i, i + 4);
         const colorKey = ColorPacker.pack(r, g, b);
         let colorIndex = colorCodeToIndex.get(colorKey);
         if (colorIndex === undefined) {
             colorIndex = colorData.length;
             colorCodeToIndex.set(colorKey, colorIndex);
             colorData.push({
-                count: 1,
-                rgbColor: [r, g, b],
+                count: a / 255,
                 oklabColor: OKlab.fromRgb(r, g, b)
             });
         }
-        colorData[colorIndex].count++;
+        colorData[colorIndex].count += a / 255;
         indexArr[i / 4] = colorIndex;
     }
     return { colorData, indexArr };
 }
 
-function naiveKmeansQuantizer(
-    data: ColorData[],
-    finalCount: number,
-    attempts: number, maxIteration = 100
-) {
+function convertIntoWorker<InType, OutType>(onMessage: (event: MessageEvent<InType>) => void) {
+    const fnStr = 'self.onmessage=' + onMessage.toString();
+    const fileBlob = new Blob([fnStr], { type: 'text/javascript' });
+    const fileUrl = URL.createObjectURL(fileBlob);
+
+    const worker = new Worker(fileUrl);
+    return worker as {
+        postMessage(message: InType): void;
+        onmessage: (message: MessageEvent<OutType>) => void;
+    };
+};
+
+type KmeansWorkerMessage = {
+    in: {
+        data: ColorData[];
+        finalCount: number;
+        attempts: number;
+        maxIteration: number;
+    };
+    out: {
+        finished: true;
+        assignment: Triple[];
+    } | {
+        finished: false;
+        progress: number;
+    };
+};
+const naiveKmeansWorker = (event: MessageEvent<KmeansWorkerMessage['in']>) => {
+    function toRgb(L: number, A: number, B: number) {
+        let l = L + A * +0.3963377774 + B * +0.2158037573;
+        let m = L + A * -0.1055613458 + B * -0.0638541728;
+        let s = L + A * -0.0894841775 + B * -1.2914855480;
+        l = l ** 3; m = m ** 3; s = s ** 3;
+        let rr = l * +4.0767416621 + m * -3.3077115913 + s * +0.2309699292;
+        let gg = l * -1.2684380046 + m * +2.6097574011 + s * -0.3413193965;
+        let bb = l * -0.0041960863 + m * -0.7034186147 + s * +1.7076147010;
+        return [rr, gg, bb].map(c => {
+            const c01 = c <= 0.0031308 ? c * 12.92 : 1.055 * Math.pow(c, 1 / 2.4) - 0.055;
+            return Math.max(Math.min(Math.round(c01 * 255), 255), 0);
+        }) as Triple;
+    }
+    const { data, finalCount, attempts, maxIteration } = event.data;
     const shuffleArray = data.map((_, i) => i);
     const allAttemptPoints: Triple[][] = [];
     for (let i = 0; i < attempts; i++) {
@@ -202,12 +225,9 @@ function naiveKmeansQuantizer(
 
     for (let iteration = 0; iteration < maxIteration; iteration++) {
         const centroids = allAttemptPoints[attempt].slice();
-        // console.log(centroids);
 
         const centroidAssignedPoints = centroids.map(() => [] as ColorData[]);
-        // Assign each point to the centroid
         for (const color of data) {
-            // Find closest centroid to that point
             const { oklabColor: [pl, pa, pb] } = color;
             let minDist = Infinity, minCentroidIndex = NaN;
             for (let i = 0; i < centroids.length; i++) {
@@ -221,7 +241,6 @@ function naiveKmeansQuantizer(
             centroidAssignedPoints[minCentroidIndex].push(color);
         }
 
-        // console.log('Centroid assigned points', centroidAssignedPoints);
         const newCentroids = centroidAssignedPoints.map((points) => {
             let totalL = 0, totalA = 0, totalB = 0;
             let totalWeight = 0;
@@ -233,21 +252,21 @@ function naiveKmeansQuantizer(
             }
             return [totalL / totalWeight, totalA / totalWeight, totalB / totalWeight] as Triple;
         });
-        // console.log('new Centroids:', newCentroids);
 
         const centroidMovements = newCentroids.map(([l0, a0, b0], i) => {
             const [l1, a1, b1] = centroids[i];
             return Math.hypot(l1 - l0, a1 - a0, b1 - b0);
         });
-        console.log('Centroid movements:\n' + centroidMovements.join('\n'));
         allAttemptPoints[attempt] = newCentroids;
+        const maxCentroidMovements = Math.max(...centroidMovements);
+        const absoluteProgress = Math.min(-Math.log10(maxCentroidMovements) / 5, 1);
+        postMessage({ finished: false, progress: absoluteProgress } satisfies KmeansWorkerMessage['out']);
 
         if (centroidMovements.every((s) => s < 0.00001)) {
             break;
         }
     }
 
-    // Assign each point to the centroid
     const centroids = allAttemptPoints[attempt];
     const assignment = data.map(({ oklabColor: [pl, pa, pb] }, i) => {
         let minDist = Infinity, minCentroidIndex = NaN;
@@ -260,12 +279,15 @@ function naiveKmeansQuantizer(
             }
         }
 
-        return OKlab.toRgb(...centroids[minCentroidIndex]);
+        return toRgb(...centroids[minCentroidIndex]);
     });
     // }
 
-    return assignment;
-}
+    postMessage({ finished: true, assignment } satisfies KmeansWorkerMessage['out']);
+    // return assignment;
+};
+
+
 
 function pruneRgbBits(pixels: Uint8ClampedArray, totalBits: number) {
     const bits = [1, 2, 0].map(i => Math.floor((totalBits + i) / 3));
@@ -283,22 +305,37 @@ function pruneRgbBits(pixels: Uint8ClampedArray, totalBits: number) {
 }
 
 async function main() {
-    const src = await UI.promptInputSrc();
+    const kMeansWorker = convertIntoWorker<KmeansWorkerMessage['in'], KmeansWorkerMessage['out']>(naiveKmeansWorker);
+
+    const { src, colorCount } = await UI.promptInputSrc();
 
     const img1 = document.createElement('img');
     await Data.loadImage(img1, src);
-    document.body.appendChild(img1);
     const pixelReader = Data.ImageArrayConverter(img1);
 
     const pixels = pixelReader.read(img1);
     pruneRgbBits(pixels, 12);
-    const pixelCounts = countColors(pixels);
     const { indexArr, colorData } = indexColors(pixels);
-    console.log(colorData);
 
-    const result = naiveKmeansQuantizer(colorData, 8, 8);
-    console.log(result);
-
+    // const result = naiveKmeansQuantizer(colorData, colorCount);
+    const progressDisplay = document.createElement('div');
+    document.body.appendChild(progressDisplay);
+    const result = await new Promise<Triple[]>((res) => {
+        kMeansWorker.onmessage = ({ data }) => {
+            if (data.finished) {
+                res(data.assignment);
+            } else {
+                progressDisplay.textContent = `Working... (${(data.progress * 100).toFixed(2)}%)`;
+            }
+        };
+        kMeansWorker.postMessage({
+            data: colorData,
+            finalCount: colorCount,
+            attempts: 8,
+            maxIteration: 100
+        });
+    });
+    document.body.removeChild(progressDisplay);
     for (let i = 0; i < indexArr.length; i++) {
         const [r, g, b] = result[indexArr[i]];
         pixels[4 * i + 0] = r;
@@ -306,7 +343,6 @@ async function main() {
         pixels[4 * i + 2] = b;
     }
 
-    UI.createLabel(`Counted ${pixelCounts.size} pixels`);
     const img2 = document.createElement('img');
     await pixelReader.write(img2, pixels);
     document.body.appendChild(img2);
