5820f6db80893bb3116f97c9395c5b8add28a620
PseudoLW <pseudolw@gmail.com>
2025-03-10 15:22:04 +0700

    refined median cut

--- src/sketch.ts
+++ src/sketch.ts
@@ -4,110 +4,79 @@ function swap(arr: unknown[], a: number, b: number) {
     arr[b] = temp;
 }
 
-
-function hoarePartition(arr: number[], lo: number, hi: number): number {
-    const pi = Math.floor((lo + hi) / 2);
-    if (arr[lo] > arr[pi]) swap(arr, lo, pi);
-    if (arr[lo] > arr[hi]) swap(arr, lo, hi);
-    if (arr[pi] > arr[hi]) swap(arr, pi, hi);
-
-    const pivotValue = arr[pi];
-
-    let i = lo - 1;
-    let j = hi + 1;
-
-    while (true) {
-        do {
-            i++;
-        } while (arr[i] < pivotValue);
-        do {
-            j--;
-        } while (arr[j] > pivotValue);
-        if (i >= j) {
-            return j;
-        }
-
-        swap(arr, i, j);
+function weightedMedian(arr: number[], lo = 0, hi = arr.length - 1): number {
+    let totalWeight = 0;
+    for (let i = lo; i <= hi; i++) {
+        totalWeight += arr[i];
     }
-}
+    let target = totalWeight / 2;
 
-function weightedMedian(
-    arr: number[],
-    lo = 0,
-    hi = arr.length - 1,
-    totalWeight = arr.reduce((a, b) => (a + b)) / 2,
-    target = totalWeight / 2
-): number {
-    console.log('Finding ');
-
-    const sliceLength = hi - lo + 1;
-    if (sliceLength === 0) {
-        throw new Error('what');
-    } else if (sliceLength === 1) {
-        if (target * 2 > arr[lo]) {
-            return lo + 1;
-        } else {
-            return lo;
-        }
-    } else if (sliceLength === 2) {
-        if (arr[lo] > arr[hi]) swap(arr, lo, hi);
-        if (target > arr[lo]) {
-            return weightedMedian(arr, lo, lo, arr[lo], target);
-        } else {
-            return weightedMedian(arr, hi, hi, arr[hi], target - arr[lo]);
-        }
-    }
-
-    const pi = Math.floor((lo + hi) / 2);
-    if (arr[lo] > arr[pi]) swap(arr, lo, pi);
-    if (arr[lo] > arr[hi]) swap(arr, lo, hi);
-    if (arr[pi] > arr[hi]) swap(arr, pi, hi);
-
-    const threshold = arr[pi];
-
-    let i = lo - 1;
-    let j = hi + 1;
-    let rightWeightSum = 0;
+    // Set up tail recursion.
     while (true) {
-        do {
-            i++;
-        } while (arr[i] < threshold);
-        do {
-            j--;
-            rightWeightSum += arr[j];
-        } while (arr[j] > threshold);
-        if (i >= j) {
-            // The new partition will start at j+1
-            rightWeightSum -= arr[j];
-            const leftWeightSum = totalWeight - rightWeightSum;
-            if (target > leftWeightSum) {
-                return weightedMedian( // Check left partition
-                    arr, lo, j,
-                    leftWeightSum, target);
-            } else {
-                return weightedMedian( // Check right partition
-                    arr, j + 1, hi,
-                    rightWeightSum, target - leftWeightSum);
+        const sliceLength = hi - lo + 1;
+
+        // Base cases
+        if (sliceLength === 0) {
+            throw new Error('Cannot find median of an empty slice.');
+        } else if (sliceLength === 1) {
+            return target * 2 > arr[lo] ? lo + 1 : lo;
+        } else if (sliceLength === 2) {
+            if (arr[lo] > arr[hi]) swap(arr, lo, hi);
+
+            if (target > arr[lo]) { // Focus on the left element.
+                return target * 2 > arr[lo] ? lo + 1 : lo;
+            } else { // Focus on the right element.
+                return (target - arr[lo]) * 2 > arr[hi] ? hi + 1 : hi;
             }
         }
 
-        swap(arr, i, j);
+        // Median of three to choose a pivot
+        const pi = Math.floor((lo + hi) / 2);
+        if (arr[lo] > arr[pi]) swap(arr, lo, pi);
+        if (arr[lo] > arr[hi]) swap(arr, lo, hi);
+        if (arr[pi] > arr[hi]) swap(arr, pi, hi);
+
+        const threshold = arr[pi];
+        let i = lo - 1, j = hi + 1;
+        let rightWeightSum = 0;
+
+        // Hoare partitioning
+        while (true) {
+            do { i++; } while (arr[i] < threshold);
+            do { j--; rightWeightSum += arr[j]; } while (arr[j] > threshold);
+
+            rightWeightSum -= arr[j];
+            if (i >= j) {
+                const leftWeightSum = totalWeight - rightWeightSum;
+                //  tail-recursively "call" the partition function again.
+                if (target < leftWeightSum) {
+                    // Focus on the left partition
+                    hi = j;
+                    totalWeight = leftWeightSum;
+                } else {
+                    // Focus on the right partition
+                    lo = j + 1;
+                    totalWeight = rightWeightSum;
+                    target -= leftWeightSum;
+                }
+                break;
+            }
+
+            swap(arr, i, j);
+            rightWeightSum += arr[j];
+        }
     }
 }
 
-function display(arr: number[], l: number, r: number, m = NaN) {
-    const wa = Array.from(' ' +
-        arr.map(s => s.toString().padStart(2, ' ')).join(' ') +
-        ' ');
-    wa[l * 3] = '[';
-    wa[r * 3 + 3] = ']';
-    if (!Number.isNaN(m)) {
+const arr = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 100));
+console.log(arr);
 
-    }
-
-    console.log(wa.join(''));
-
+const partition = weightedMedian(arr);
+console.log(`Partitioned into medians at ${partition}.`);
+let l2 = 0, r2 = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (i < partition) l2 += arr[i]; else r2 += arr[i];
 }
+console.log(arr);
 
-const arr = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100));
-display(arr, 0, arr.length - 1);
+console.log(`Weight on the left: ${l2} vs right: ${r2}`);
